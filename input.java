cfe7ea9dcb1742db840264f9bea161e313fb1f8d
Suriya-Prakashar <suriyaprakashar15751@gmail.com>
2026-01-09 21:05:12 +0530

    adding java file

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class input{
+
+public static void main(String[] agrs){
+    Scanner Scan = new Scanner(System.in);
+    String name = Scan.nextLine();
+    int age = Scan.nextInt();
+    System.out.println("my name is :"+name);
+    System.out.println("my age is :"+age);
+
+}
+}
